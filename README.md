1d4813487e93c22456b12b834767b9f57b1681e9
Gilson Rodrigues de Almeida <83088082+GilsonRodriguesAlmeida@users.noreply.github.com>
2024-03-01 14:07:27 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Projeto do https://curso.dev para implementação de uma aplicação em todos os processos, clone do projeto https://www.tabnews.com.br
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,9 @@
 function Home() {
-    return(
-        <h1>Daiane meu amor, eu te amo s2! se você me ama da uma risadinha 😍!!!</h1>
-    )
+  return (
+    <h1>
+      Daiane meu amor, eu te amo s2! se você me ama da uma risadinha 😍!!!
+    </h1>
+  );
 }
 
 export default Home;
